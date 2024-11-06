a4ef9b7d3f4e94b2ac7cdc7b8e607b92d434b089
Felix Hauser <felix.hauser@kit.edu>
2024-11-06 18:07:08 +0100

    Fix init imports

--- nc_toolbox/__init__.py
+++ nc_toolbox/__init__.py
@@ -1,8 +1,9 @@
 from .statistic import class_embedding_means, class_embedding_variances
 from .statistic import global_embedding_mean
 from .statistic import split_embeddings, center_embeddings
+from .statistic import between_class_covariance, within_class_covariance
 
-from .metrics import nc1_cdnv
+from .metrics import nc1_strong, nc1_weak, nc1_cdnv
 from .metrics import nc2_equinormness, nc2_equiangularity, gnc2_hypershperical_uniformity
 from .metrics import nc3_self_duality, unc3_uniform_duality
 from .metrics import nc4_classifier_agreement
