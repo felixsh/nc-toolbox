ad17069423daab712cb5aaacee874c1a669fbbb6
Felix Hauser <felix.hauser@kit.edu>
2025-02-05 15:21:11 +0100

    Fix wildcard importing in init.py

--- nc_toolbox/__init__.py
+++ nc_toolbox/__init__.py
@@ -1,3 +1,5 @@
+"""A package for calculating Neural Collapse and related metrics."""
+
 from .decomp import principal_decomp
 from .metrics import (
     gnc2_hypershperical_uniformity,
@@ -20,3 +22,25 @@ from .statistic import (
     within_class_covariance,
 )
 from .util import lin_classify, ncc_classify
+
+__all__ = [
+    'principal_decomp',
+    'gnc2_hypershperical_uniformity',
+    'nc1_cdnv',
+    'nc1_strong',
+    'nc1_weak',
+    'nc2_equiangularity',
+    'nc2_equinormness',
+    'nc3_self_duality',
+    'nc4_classifier_agreement',
+    'unc3_uniform_duality',
+    'between_class_covariance',
+    'center_embeddings',
+    'class_embedding_means',
+    'class_embedding_variances',
+    'global_embedding_mean',
+    'split_embeddings',
+    'within_class_covariance',
+    'lin_classify',
+    'ncc_classify',
+]
